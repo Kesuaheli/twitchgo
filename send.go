48e5292c15989e0a47566146eb600bc303afda59
Kesuaheli <kesuaheligame@gmail.com>
2023-12-23 14:42:12 +0100

    Added join channel function

--- send.go
+++ send.go
@@ -6,28 +6,43 @@ import (
 	"strings"
 )
 
+// SendCommandf formats according to a format specifier and sends the resulting command to twitch
 func (t *Twitch) SendCommandf(format string, a ...any) {
 	t.SendCommand(fmt.Sprintf(format, a...))
 }
 
-func (t *Twitch) SendCommand(msg string) {
-	if len(msg) == 0 {
+// SendCommand sends the given command to twitch
+func (t *Twitch) SendCommand(cmd string) {
+	cmd = strings.TrimSuffix(cmd, "\n") + "\r\n"
+	if len(cmd) == 2 {
 		return
 	}
-	msg = fmt.Sprintf("%s\r\n", msg)
-	_, err := t.conn.Write([]byte(msg))
+	_, err := t.conn.Write([]byte(cmd))
 	if err != nil {
-		log.Printf("failed to send command '%s': %+v", msg, err)
+		log.Printf("failed to send command '%s': %+v", cmd, err)
 		return
 	}
-	log.Printf("<< %s", msg)
+	if !strings.HasPrefix(cmd, string(MsgCmdPass)) {
+		log.Printf("<< %s", cmd)
+	} else {
+		log.Printf("<< %s ***", MsgCmdPass)
+	}
 }
 
+// SendMessagef formats according to a format specifier and sends the resulting message to the given
+// channel
 func (t *Twitch) SendMessagef(channel, format string, a ...any) {
 	t.SendMessage(channel, fmt.Sprintf(format, a...))
 }
 
+// SendMessage sends a message to the given channel
 func (t *Twitch) SendMessage(channel, msg string) {
 	channel, _ = strings.CutPrefix(channel, "#")
 	t.SendCommandf("%s #%s :%s", MsgCmdPrivmsg, channel, msg)
+}
+
+// JoinChannel joins the given channel and receives messages from that channel afterwards
+func (t *Twitch) JoinChannel(channel string) {
+	channel, _ = strings.CutPrefix(channel, "#")
+	t.SendCommandf("%s #%s", MsgCmdJoin, channel)
 }
